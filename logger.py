e595c2d424622a58cfe751c921655b9a417d9269
SergeiPospelov <pospelka413@gmail.com>
2022-06-18 21:10:21 +0300

    Добавлена работа с удаленными файлами

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 from datetime import datetime as dt
 
-data = {'deal_id': 1, 'deal': 'Помыть кота', 'deadline': '12.05.2022', 'status': 'выполнено'}
+dict = {1: 'Новое', 2: 'В работе', 3: "Отложенное", 4: "Просроченное", 5: "Выполненное"}
 
 
 def add(data, operation):
@@ -10,8 +10,8 @@ def add(data, operation):
         b = data['deal']
         c = data['deadline']
         d = data['status']
-        file.write(f"{a}) {b}. {c}. {d}. {operation}{time}")
+        if d != 10:
+            file.write(f"{a}) {b}. {c}. {dict[d]}. |{operation} {time}|\n")
+        else:
+            file.write(f"{a}) {b}. {c}. |{operation}' '{time}|\n")
     return
-
-
-add(data, 'Изменено')
