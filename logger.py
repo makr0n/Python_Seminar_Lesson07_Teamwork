22a4e6b4e1357ecd8d2a90158061765e64aa16e2
SergeiPospelov <pospelka413@gmail.com>
2022-06-18 12:32:26 +0300

    changed name

--- logger.py
+++ logger.py
@@ -1,8 +1,17 @@
 from datetime import datetime as dt
 
+data = {'deal_id': 1, 'deal': 'Помыть кота', 'deadline': '12.05.2022', 'status': 'выполнено'}
+
 
 def add(data, operation):
     time = dt.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("log.txt", "a", encoding = 'utf8') as file:
-        file.write(f"{data}; {operation}; {time} \n")
+    with open("log.txt", "a", encoding='utf8') as file:
+        a = data['deal_id']
+        b = data['deal']
+        c = data['deadline']
+        d = data['status']
+        file.write(f"{a}) {b}. {c}. {d}. {operation}{time}")
     return
+
+
+add(data, 'Изменено')
