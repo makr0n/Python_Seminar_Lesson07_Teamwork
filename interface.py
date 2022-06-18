5ba7edee91f533835c33f3e630b262ff636b71eb
Rillout87 <fedkv2013@gmail.com>
2022-06-18 12:29:16 +0300

    case 4 ch

--- interface.py
+++ interface.py
@@ -5,10 +5,11 @@ import time
 main_menu = \
     'Выберите пункт меню:\n\
     1. \033[4mСписок всех дел\033[0m\n\
-    2. \033[4mСписок дел по статусу\033[0m\n\
-    3. \033[4mДобавить дело\033[0m\n\
-    4. \033[4mИзменить дело\033[0m\n\
-    5. \033[4mВыход\033[0m'
+    2. \033[4mСписок незавершенных дел\033[0m\n\
+    3. \033[4mСписок завершенных дел\033[0m\n\
+    4. \033[4mДобавить дело\033[0m\n\
+    5. \033[4mИзменить дело\033[0m\n\
+    6. \033[4mВыход\033[0m'
 
 
 def start_page():  # Starting page, choose number
@@ -24,7 +25,7 @@ def start_page():  # Starting page, choose number
 
 
 def show_deals(data):  # 1 in menu
-
+    # os.system('cls')
     if data != []:
 
         print('\033[4mСписок всех дел:\033[0m')
@@ -39,14 +40,13 @@ def show_deals(data):  # 1 in menu
         print('\033[33mСписок дел пуст\033[0m')
 
 
-def show_deals_by_status():
-    print('\033[4mВыберите статус дела для отображения:\033[0m')
-    status = (input('1-Новые, 2-В работе, 3-Отложенные, 4-Просроченные, 5-Выполенные'))
+def unfinished_deals():
+    print('\033[4mСписок текущих дел:\033[0m')
+    print(data)
     print(50 * "=")
-    return status
 
 
-def finished_deals():
+def finished_deals():  # 4 in menu
     print('\033[4mСписок законченных дел:\033[0m')
     print(data)
     print(50 * "=")
@@ -58,28 +58,17 @@ def add_deal():
     print(50 * "-")
     deal_name = input('Что необходимо сделать: ')  # plain text
     deal_deadline = input('Укажите сроки выполнения в формате ДД-ММ-ГГ: ')  # DD-MM-YY
-    deal = {'deal_id': -1, 'deal': deal_name, 'deadline': deal_deadline, 'status': 1}
-    deal['status'] = 'Новое'
+    deal = deal_name + deal_deadline
     return deal
 
 
 def change_deal():
     print('\033[4mИзменение дела:\033[0m')
     print(50 * "~")
+    show_deals(data)
     deal_id = input('Выберите номер дела которого необходимо изменить:')
-    return deal_id
-
-
-def change_deal(one_deal):
-    command = input('Что необходимо сделать:\n 1 - Изменить статус \n 2 - Изменить содержание \n 3 - Удалить дело')
-    if command == 1:
-        status = (input('1-Новые, 2-В работе, 3-Отложенные, 4-Просроченные, 5-Выполенные'))
-        one_deal['status'] = int(status)
-    elif command == 2:
-        one_deal['deal'] = (input('Новое описание дела: '))
-    else command == 3:
-        one_deal['status'] = 10 #Статус дела на удаление
-    return one_deal
+    command = input('Что необходимо сделать:\n 1 - Завершить дело\n 2 - Изменить содержание \n 3 - Удалить дело')
+    return command, deal_id
 
 
 def bye_mess():  # 6 in menu
@@ -91,7 +80,3 @@ def error_input():
     print('\033[21mПожалуйста введите число, соответствующее пункту меню.\033[0m')
     time.sleep(2)
     start_page()
-
-
-def done_message():
-    print('Выполнено!')
